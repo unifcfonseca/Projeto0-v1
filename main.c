b06c4a3e29ed44b29e8d3e74132683afeb15871a
pedroosabainsk <160777874+pedroosabainsk@users.noreply.github.com>
2024-03-20 18:55:43 +0000

    menu 2.0

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main(void) {
+  int opcao;
+  do {
+    printf("\nMenu Principal\n");
+    printf("1 - Criar Tarefa\n");
+    printf("2 - Deletar Tarefa\n");
+    printf("3 - Listar Tarefas\n");
+    printf("0 - Sair\n");
+    scanf("%d", &opcao);
+    printf("Opção: %d\n", opcao);
+
+  } while (opcao != 0);
+}
