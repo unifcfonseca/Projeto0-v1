781a3a972551f00de4aad30b92888822627b0d9f
Guilherme-Antoniassi <145165615+Guilherme-Antoniassi@users.noreply.github.com>
2024-03-20 19:05:08 +0000

    commitDoGui

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main(){
+int opcao;
+  do(
+    printf("\n Menu Principal");
+    printf("1= Criar tarefa");
+  printf("2- deletar tarefa");
+  printf("3- listar tarefa");
+  printf("0-sair\n ");
+  printf(" escolha um opcao");
+  scanf("%d", &opcao);
+  printf("opcao escolhida : %d\n",opcao)
+  
+  )
+    while(opcao !=0);
+        
+  }
