cf177064fd228c9d0582e399c5b0bf3509d6d7ef
Guilherme-Antoniassi <145165615+Guilherme-Antoniassi@users.noreply.github.com>
2024-03-22 20:49:57 +0000

    ola

--- main.c
+++ main.c
@@ -1,34 +1,23 @@
 #include <stdio.h>
+<include <tarefas.h>
+//tarefa.c-------------------------------------------------------------------
+Erro criar(tarefa t[], int *pos){
+  if(*pos>=TOTAL)
+    return MAX_TAREFA;
 
-<<<<<<< HEAD
-int main(){
-int opcao;
-  do(
-    printf("\n Menu Principal");
-    printf("1= Criar tarefa");
-  printf("2- deletar tarefa");
-  printf("3- listar tarefa");
-  printf("0-sair\n ");
-  printf(" escolha um opcao");
-  scanf("%d", &opcao);
-  printf("opcao escolhida : %d\n",opcao)
-  
-  )
-    while(opcao !=0);
-        
-  }
-=======
-int main(void) {
-  int opcao;
-  do {
-    printf("\nMenu Principal\n");
-    printf("1 - Criar Tarefa\n");
-    printf("2 - Deletar Tarefa\n");
-    printf("3 - Listar Tarefas\n");
-    printf("0 - Sair\n");
-    scanf("%d", &opcao);
-    printf("Opção: %d\n", opcao);
+  printf("entre com uma prioridade: ");
+  scanf("%d", &t[*pos].prioridade);
 
-  } while (opcao != 0);
+  printf("entre com a descricao :");
+  fgets(t[*pos].descriacao, 30, stdin);
+
+  printf("entre com a categoria :");
+  fgets(t[*posicao].categoria,100,stdin);
+
+  printtarefa(t[*pos]);
+  *pos=*pos+1;
 }
->>>>>>> b06c4a3e29ed44b29e8d3e74132683afeb15871a
+
+int main(){
+
+  }
