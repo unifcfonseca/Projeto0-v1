b68ab9ec8381b2d7d34cb65cd04807f226805de4
Guilherme-Antoniassi <145165615+Guilherme-Antoniassi@users.noreply.github.com>
2024-03-27 18:41:48 +0000

    main atualizada

---
+++ main.c
@@ -0,0 +1,48 @@
+#include "tarefas.h"
+#include <stdio.h>
+
+int main() {
+  int opcao;
+
+  Tarefa tarefas[TOTAL];
+  int pos = 0;
+  Erro e = carregar(tarefas, &pos, TOTAL);
+  if (e == ABRIR || e == LER || e == FECHAR) {
+    pos = 0;
+    printf("Erro ao carregar as tarefas\n");
+  }
+
+  do {
+    printf("\nMenu principal\n");
+    printf("1 - Criar tarefa\n");
+    printf("2 - Deletar tarefa\n");
+    printf("3 - Listar tarefas\n");
+    printf("0 - Sair\n");
+    printf("Escolha uma opcao: ");
+    scanf("%d", &opcao);
+    printf("Opcao escolhida: %d\n", opcao);
+
+    if (opcao == 1) {
+      e = criar(tarefas, &pos);
+      if (e == MAX_TAREFAS)
+        printf("Maximo de tarefas atingido\n");
+    } else if (opcao == 2) {
+      e = deletar(tarefas, &pos);
+      if (e == SEM_TAREFAS)
+        printf("Nenhuma tarefa para ser deletada\n");
+      else if (e == NAO_ENCONTRADO)
+        printf("Tarefa nao encontrada na lista\n");
+    } else if (opcao == 3) {
+      e = listar(tarefas, pos);
+      if (e == SEM_TAREFAS)
+        printf("Nao existem tarefas para listar\n");
+    } else if (opcao == 0) {
+      printf("Sair...\n");
+      e = salvar(tarefas, pos, TOTAL);
+      if (e == ABRIR || e == ESCREVER || e == FECHAR)
+        printf("Nao foi possivel salvar as tarefas\n");
+    } else
+      printf("Opcao invalida\n");
+
+  } while (opcao != 0);
+}
